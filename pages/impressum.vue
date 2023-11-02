92a8fd770b3869908563ee142ef4cb1cb36c6353
patrickbruckner <patrick.bruckner@gmail.com>
2023-11-02 17:11:33 +0100

    Impressum finished

---
+++ pages/impressum.vue
@@ -0,0 +1,98 @@
+<script setup lang="ts">
+definePageMeta({ layout: 'page' })
+useHead({ titleTemplate: '', title: 'Impressum - Klare Köpfe Ybbs' })
+</script>
+
+<template>
+  <LayoutPageWrapper class="grid gap-5">
+    <LayoutPageSectionTitle> Impressum </LayoutPageSectionTitle>
+
+    <AwesomeCard>
+      <AwesomeCardContent class="grid gap-2">
+        <p>
+          <b
+            >Informationen und Offenlegung gemäß &sect;5 (1) ECG, &sect; 25
+            MedienG, &sect; 63 GewO und &sect; 14 UGB</b
+          >
+        </p>
+        <p><b>Webseitenbetreiber:</b> Thomas Pöchhacker</p>
+        <p><b>Anschrift:</b> Klostermühlstrasse 1, 3370 Ybbs an der Donau</p>
+        <p>
+          <b>Gewerbeaufsichtbehörde:</b> Bezirkshauptmannschaft Melk <br />
+          <b>Mitgliedschaften:</b>
+        </p>
+        <p>
+          <b>Kontaktdaten:</b> <br />
+
+          Telefon:
+          <AwesomeLink href="tel:00436643337084">
+            +43 664 / 333 70 84
+          </AwesomeLink>
+          <br />
+          Email:
+          <AwesomeLink href="mailto:info@klarekoepfeybbs.at">
+            info@klarekoepfeybbs.at
+          </AwesomeLink>
+          <br />
+        </p>
+      </AwesomeCardContent>
+    </AwesomeCard>
+
+    <AwesomeCard>
+      <AwesomeCardContent class="grid gap-2">
+        <p>
+          <b>Anwendbare Rechtsvorschrift:</b> www.ris.bka.gv.at <br />
+          <b>Berufsbezeichnung:</b> Non-Profit-Vereinigung zum
+          Informationsaustausch und Verbesserung der Lebensqualität in der
+          Stadtgemeinde Ybbs an der Donau
+        </p>
+        <p>
+          <b>Online Streitbeilegung:</b> Verbraucher, welche in Österreich oder
+          in einem sonstigen Vertragsstaat der ODR-VO niedergelassen sind, haben
+          die Möglichkeit Probleme bezüglich dem entgeltlichen Kauf von Waren
+          oder Dienstleistungen im Rahmen einer Online-Streitbeilegung (nach OS,
+          AStG) zu lösen. Die Europäische Kommission stellt eine Plattform
+          hierfür bereit: https://ec.europa.eu/consumers/odr
+        </p>
+        <p>
+          <b>Urheberrecht:</b> Die Inhalte dieser Webseite unterliegen, soweit
+          dies rechtlich möglich ist, diversen Schutzrechten (z.B dem
+          Urheberrecht). Jegliche Verwendung/Verbreitung von bereitgestelltem
+          Material, welche urheberrechtlich untersagt ist, bedarf schriftlicher
+          Zustimmung des Webseitenbetreibers.
+        </p>
+        <p>
+          <b>Haftungsausschluss:</b> Trotz sorgfältiger inhaltlicher Kontrolle
+          übernimmt der Webseitenbetreiber dieser Webseite keine Haftung für die
+          Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind
+          ausschließlich deren Betreiber verantwortlich. Sollten Sie dennoch auf
+          ausgehende Links aufmerksam werden, welche auf eine Webseite mit
+          rechtswidriger Tätigkeit/Information verweisen, ersuchen wir um
+          dementsprechenden Hinweis, um diese nach § 17 Abs. 2 ECG umgehend zu
+          entfernen.<br />Die Urheberrechte Dritter werden vom Betreiber dieser
+          Webseite mit größter Sorgfalt beachtet. Sollten Sie trotzdem auf eine
+          Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
+          entsprechenden Hinweis. Bei Bekanntwerden derartiger
+          Rechtsverletzungen werden wir den betroffenen Inhalt umgehend
+          entfernen.
+        </p>
+      </AwesomeCardContent>
+    </AwesomeCard>
+
+    <div class="text-sm">
+      Quelle:
+      <b
+        ><a
+          href="https://www.rechtstexte-generator.at/dsgvo-generator-oesterreich/"
+          >Datenschutz DSGVO Generator</a
+        ></b
+      >
+      in Kooperation mit <br /><b
+        ><a
+          href="https://www.ortner-rechtsanwalt.at/rechtsanwalt-eherecht-scheidungsrecht-obsorgerecht/"
+          >Rechtsanwalt Wien Scheidung</a
+        ></b
+      >
+    </div>
+  </LayoutPageWrapper>
+</template>
