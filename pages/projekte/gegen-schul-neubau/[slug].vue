1366cd33724496377d84ecabe9a4ae7480d9c1fa
patrickbruckner <patrick.bruckner@gmail.com>
2023-11-12 13:34:40 +0100

    Bürgerforum einbinden

---
+++ pages/projekte/gegen-schul-neubau/[slug].vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+definePageMeta({ layout: 'page' })
+
+const { page } = useContent()
+</script>
+
+<template>
+  <LayoutPageWrapper id="content" class="space-y-10">
+    <!-- <ContentDoc /> -->
+    <ContentRenderer v-if="page" :key="page._id" :value="page" />
+
+    <div v-else>
+      <LayoutPageSectionTitle>
+        <span class="text-2xl font-bold">Inhalt nicht gefunden.</span>
+      </LayoutPageSectionTitle>
+
+      Leider konnte der gewünschte Inhalt nicht gefunden werden. Hier kommen sie
+      zurück zum Projekt.
+    </div>
+
+    <AwesomeButton to="/projekte/gegen-schul-neubau" class="mt-4">
+      zum Projekt
+    </AwesomeButton>
+  </LayoutPageWrapper>
+</template>
+
+<style scoped>
+#content :deep(h1) {
+  @apply text-2xl font-semibold mb-2;
+}
+
+#content :deep(h2) {
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+  @apply text-xl font-semibold mb-2;
+}
+
+#content :deep(p) {
+  margin-bottom: 1.5rem;
+}
+</style>
--- pages/projekte/gegen-schul-neubau/index.vue
+++ pages/projekte/gegen-schul-neubau/index.vue
@@ -7,6 +7,16 @@ useHead({ titleTemplate: '', title: 'Gegen den Schul-Neubau' })
   <LayoutPageWrapper class="space-y-10">
     <ProjectSchoolHeader />
 
+    <LayoutPageSectionTitle>
+      <span class="text-2xl font-bold">Bürgerforum am 3.11.2023</span>
+    </LayoutPageSectionTitle>
+
+    Wie das Bürgerforum abgelaufen ist, erfahren Sie hier.
+
+    <AwesomeButton to="/projekte/gegen-schul-neubau/buergerforum">
+      zum Bericht
+    </AwesomeButton>
+
     <ProjectSchoolArguments id="argumente" />
 
     <!-- Kosten -->
